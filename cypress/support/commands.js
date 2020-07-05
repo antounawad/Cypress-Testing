5f8462b87193baf53245ae0fec027474f5f19571
Antoun <antoun@awad.eg>
2020-07-05 18:41:50 +0200

    add custome commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,34 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command"
+
+addMatchImageSnapshotCommand()
+
+// define a custome commands in order to use it later as a short cut in your tests
+
+Cypress.Commands.add("isVisable", selector => {
+	cy.get(selector).should("be.visible")
+})
+
+Cypress.Commands.add("isHidden", selector => {
+	cy.get(selector).should("not.exist")
+})
+
+Cypress.Commands.add("setResolution", size => {
+	// Cypress includes Lodash utility library
+	if (Cypress._.isArray(size)) {
+		cy.viewport(size[0], size[1])
+	} else {
+		cy.viewport[size]
+	}
+})
+
+Cypress.Commands.add("login", (username, password) => {
+	cy.get("#login_form").should("be.visible")
+	cy.get("#user_login").type(username)
+	cy.get("#user_password").type(password)
+	cy.get("#user_remember_me").click()
+	cy.contains("Sign in").click()
+})
