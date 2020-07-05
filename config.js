e1bae7f24041cd2c2ade669ad5e377059e4a3f0c
Antoun <antoun@awad.eg>
2020-07-05 13:34:32 +0200

    add configjs file to define project configs

---
+++ config.js
@@ -0,0 +1,6 @@
+// general project configs should go here
+// you can set these config also in the Cypress.json but here
+// u have more contorl and can set multiple config for the same prob . like more urls
+export const url = "http://zero.webappsecurity.com/"
+export const login_username = "username"
+export const login_password = "password"
